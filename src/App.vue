94b0f99842ccd0ba05e3be6be2d00a8722893a45
leelsmuth <femianjorin@rocketmail.com>
2020-11-18 22:26:14 -0500

    template conditionals, loops and events

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  display: flex;
   min-height: 100vh;
   background-color: #f3f5fa;
 }
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="user-profile">
-    <div class="user-profile_user-panel">
-      <h1 class="user-profile_user-name">@{{ user.username }}</h1>
-      <div class="user-profile_follower">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__user-name">@{{ user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+      <div class="user-profile__follower">
         <strong>Followers: </strong> {{ followers }}
       </div>
-      <button @click="followUser()">Click</button>
+    </div>
+    <div class="user-profile__twoots-wrapper">
+      <TwootItem
+        v-for="twoot in user.twoots"
+        :key="twoot.id"
+        :username="user.username"
+        :twoot="twoot"
+        @favourite="toggleFavourite"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import TwootItem from "./TwootItem";
+
 export default {
   name: "UserProfile",
+  components: { TwootItem },
   data() {
     return {
       followers: 0,
@@ -23,6 +35,10 @@ export default {
         lastName: "Anjorin",
         emial: "femianjorin@rocketmail.com",
         isAdmin: true,
+        twoots: [
+          { id: 1, content: "Twotter is Amazing!" },
+          { id: 2, content: "Don't forget to Subscribe to Our Channel" },
+        ],
       },
     };
   },
@@ -42,6 +58,9 @@ export default {
     followUser() {
       this.followers++;
     },
+    toggleFavourite(id) {
+      console.log(`Favourited Tweet #${id}`);
+    },
   },
   mounted() {
     this.followUser();
@@ -50,4 +69,39 @@ export default {
 </script>
 
 <style>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+}
+
+h1 {
+  margin: 0;
+}
+
+.user-profile__admin-badge {
+  background-color: purple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+
+.user-profile__twoots-wrapper {
+  display: grid;
+  grid-gap: 10px;
+  width: 80%;
+}
 </style>
